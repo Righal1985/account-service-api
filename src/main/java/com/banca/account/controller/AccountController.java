c07181c3abd04745831272f1f7a031b107f834b8
wizard <elwizard16@gmail.com>
2026-06-17 10:11:48 -0400

    feat: implementar endpoints de consulta y creacion en AccountController

--- src/main/java/com/banca/account/controller/AccountController.java
+++ src/main/java/com/banca/account/controller/AccountController.java
@@ -1,15 +1,35 @@
 package com.banca.account.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.banca.account.model.Account;
+import com.banca.account.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/accounts")
 public class AccountController {
 
+    @Autowired
+    private AccountRepository accountRepository;
+
+    // Tu endpoint original de prueba
     @GetMapping("/test")
     public String checkSystem() {
         return "Conexión exitosa: El motor bancario está listo.";
+    }
+
+    // Listar todas las cuentas
+    @GetMapping
+    public List<Account> getAllAccounts() {
+        return accountRepository.findAll();
+    }
+
+    // Crear una nueva cuenta
+    @PostMapping
+    public Account createAccount(@RequestBody Account account) {
+        return accountRepository.save(account);
     }
 }
